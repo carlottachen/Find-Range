07d0c76c5ede6b656e400884cffae55a634c363c
Carlotta Chen <carlottachen92@gmail.com>
2021-11-11 08:52:15 -0800

    Initial commit

--- findTheRange.js
+++ findTheRange.js
@@ -4,7 +4,7 @@
 const array1 = [1, 4, 11, 2, 37, -4];
 
 //sort array by ascending order
-array1.sort(function(a, b){return a-b});
+array1.sort(function(min, max){return min-max});
 
 //console.log(array1);
 
@@ -18,3 +18,23 @@ for(var i = 0; i < array1.length + 1; i++){
 	console.log(array1[i - 1]);
     }
 }
+
+/* Instructor's solution to this code */
+
+//Starting the array
+let array = [23, 43, -12, 30, 4, 0, -36];
+//start at the beginning of the array
+//these will hold the smallest/largest numbers
+let smallest = array[0];
+let largest = array[0];
+//loop through array, if array[i] is smaller
+//or larger, then replace variable to be displayed
+for(let i = 0; i < array.length; i++){
+	if(smallest > array[i]){
+		smallest = array[i];
+	}else if(largest < array[i]){
+		largest = array[i];
+	}
+}
+//display variables
+console.log(smallest, largest);
